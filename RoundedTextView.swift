060e50d2405d5345da6059baabbcf01d39d42a33
Andrew C. Friedman <andrew.friedman@hey.com>
2021-05-12 09:47:48 -0400

    Make RoundedTextView draggable within its superview

--- RoundedTextView.swift
+++ RoundedTextView.swift
@@ -16,6 +16,8 @@ class RoundedTextView: NSView {
     
     private var textField: NSTextField!
     
+    private var lastDragLocation: NSPoint?
+    
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
         
@@ -57,5 +59,28 @@ class RoundedTextView: NSView {
             textField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
             textField.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor, constant: 8),
         ])
+    }
+    
+    // MARK: Mouse Events
+    
+    override func acceptsFirstMouse(for event: NSEvent?) -> Bool {
+        return true
+    }
+    
+    override func mouseDown(with event: NSEvent) {
+        lastDragLocation = superview?.convert(event.locationInWindow, from: nil)
+    }
+    
+    override func mouseDragged(with event: NSEvent) {
+        
+        guard let newDragLocation = superview?.convert(event.locationInWindow, from: nil),
+              let lastDragLocation = lastDragLocation else {
+            return
+        }
+        var thisOrigin = frame.origin
+        thisOrigin.x += -lastDragLocation.x + newDragLocation.x
+        thisOrigin.y += -lastDragLocation.y + newDragLocation.y
+        setFrameOrigin(thisOrigin)
+        self.lastDragLocation = newDragLocation
     }
 }
