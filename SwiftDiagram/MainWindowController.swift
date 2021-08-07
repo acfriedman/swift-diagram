55a5899e9f23e4c44f12971d5d65f5005d27a329
Andrew C. Friedman <andrew.friedman@hey.com>
2021-08-07 14:30:25 -0400

    Add a search bar item to the tool bar

--- SwiftDiagram/MainWindowController.swift
+++ SwiftDiagram/MainWindowController.swift
@@ -24,12 +24,8 @@ class MainWindowController: NSWindowController {
             newToolbar.allowsUserCustomization = true
             newToolbar.autosavesConfiguration = true
             newToolbar.displayMode = .default
+            newToolbar.centeredItemIdentifier = NSToolbarItem.Identifier.toolbarSearchItem
             
-            // Example on center-pinning a toolbar item
-            newToolbar.centeredItemIdentifier = NSToolbarItem.Identifier.toolbarPickerItem
-            
-            // Hiding the title visibility in order to gain more toolbar space.
-            // Set this property to .visible or delete this line to get it back.
             unwrappedWindow.titleVisibility = .hidden
             unwrappedWindow.toolbar = newToolbar
             unwrappedWindow.toolbar?.validateVisibleItems()
@@ -39,6 +35,77 @@ class MainWindowController: NSWindowController {
 
 extension MainWindowController: NSToolbarDelegate {
     
+    func toolbar(_ toolbar: NSToolbar,
+                 itemForItemIdentifier itemIdentifier: NSToolbarItem.Identifier,
+                 willBeInsertedIntoToolbar flag: Bool) -> NSToolbarItem? {
+        
+        if  itemIdentifier == NSToolbarItem.Identifier.toolbarSearchItem {
+            let searchItem = NSSearchToolbarItem(itemIdentifier: itemIdentifier)
+            searchItem.resignsFirstResponderWithCancel = true
+            searchItem.searchField.delegate = self
+            searchItem.toolTip = "Search"
+            return searchItem
+        }
+        
+        return nil
+    }
+    
+    func toolbarDefaultItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
+        return [
+            NSToolbarItem.Identifier.toolbarItemToggleTitlebarAccessory,
+            NSToolbarItem.Identifier.toolbarItemUserAccounts,
+            NSToolbarItem.Identifier.toolbarItemMoreInfo,
+            NSToolbarItem.Identifier.flexibleSpace,
+            NSToolbarItem.Identifier.toolbarPickerItem,
+            NSToolbarItem.Identifier.flexibleSpace,
+            NSToolbarItem.Identifier.toolbarMoreActions,
+            NSToolbarItem.Identifier.toolbarShareButtonItem,
+            NSToolbarItem.Identifier.toolbarSearchItem
+        ]
+        
+    }
+    
+    func toolbarAllowedItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
+        return [
+            NSToolbarItem.Identifier.toolbarItemToggleTitlebarAccessory,
+            NSToolbarItem.Identifier.toolbarMoreActions,
+            NSToolbarItem.Identifier.toolbarItemUserAccounts,
+            NSToolbarItem.Identifier.toolbarItemMoreInfo,
+            NSToolbarItem.Identifier.toolbarPickerItem,
+            NSToolbarItem.Identifier.toolbarShareButtonItem,
+            NSToolbarItem.Identifier.toolbarSearchItem,
+            NSToolbarItem.Identifier.space,
+            NSToolbarItem.Identifier.flexibleSpace]
+    }
+    
+    func toolbarWillAddItem(_ notification: Notification)
+    {
+        // print("~ ~ toolbarWillAddItem: \(notification.userInfo!)")
+    }
+    
+    func toolbarDidRemoveItem(_ notification: Notification)
+    {
+        // print("~ ~ toolbarDidRemoveItem: \(notification.userInfo!)")
+    }
+    
+    func toolbarSelectableItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier]
+    {
+        // Return the identifiers you'd like to show as "selected" when clicked.
+        // Similar to how they look in typical Preferences windows.
+        return []
+    }
+    
+}
+
+extension MainWindowController: NSSearchFieldDelegate {
+    func searchFieldDidStartSearching(_ sender: NSSearchField) {
+        print("Search field did start receiving input")
+    }
+    
+    func searchFieldDidEndSearching(_ sender: NSSearchField) {
+        print("Search field did end receiving input")
+        sender.resignFirstResponder()
+    }
 }
 
 extension NSToolbar.Identifier {
